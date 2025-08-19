1810d86dbadb84a37eb108078d2e0f5d5a4d0c46
FMN707 <muhammadamin071011@icloud.com>
2025-08-19 14:08:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let h1 = document.querySelector('h1')
+let p = document.querySelector('p')
+
+
+new Typed(h1,{
+    strings:['TEXT GRADIENTS'],
+    typeSpeed:50,
+    backSpeed:50,
+    startDelay:100,
+    loop:true,
+})
+
+new Typed(p,{
+    strings:['Make your headlines stand out'],
+    typeSpeed:50,
+    backSpeed:50,
+    startDelay:100,
+    loop:true,
+})
